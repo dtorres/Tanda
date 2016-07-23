c23d6f12b72db6fe2fcb8eb0c55607809fe40949
Diego Torres <warorface@gmail.com>
2016-07-23 17:46:14 +0200

    Initial Commit

---
+++ Tanda/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Tanda
+//
+//  Created by Diego Torres on 23/07/16.
+//  Copyright © 2016 Diego Torres. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
